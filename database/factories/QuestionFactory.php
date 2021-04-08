8a013b8bb63898f3b09794998f4b5bffebbebb4e
Jámbor Alexandra <ja20000907@gmail.com>
2021-04-08 15:21:28 +0200

    Generated Question files

---
+++ database/factories/QuestionFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Question;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionFactory extends Factory
+{
+    protected $model = Question::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph($this->faker->numberBetween(3, 15)),
+            'score' => $this->faker->boolean(70) ? $this->faker->numberBetween(1, 100_000) : 0,
+            'views' => $this->faker->boolean(70) ? $this->faker->numberBetween(1, 100_000) : 0,
+        ];
+    }
+}
