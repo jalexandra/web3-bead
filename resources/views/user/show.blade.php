adddd415e2a7d59fe1340bb7128d62441e9cab62
Jámbor Alexandra <ja20000907@gmail.com>
2021-04-17 20:53:33 +0200

    Implemented User Page

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,5 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    {{ $user }}
+    <?php
+        use App\Models\User;
+        /** @var User $user */
+    ?>
+    <div>
+        <table>
+            <tbody>
+                <tr>
+                    <td>ID: </td>
+                    <td>{{ $user->id }}</td>
+                </tr>
+                <tr>
+                    <td>Username: </td>
+                    <td>{{ $user->username }}</td>
+                </tr>
+                <tr>
+                    <td>Email: </td>
+                    <td>{{ $user->email }}</td>
+                </tr>
+                <tr>
+                    <td>XP: </td>
+                    <td>{{ $user->xp }}</td>
+                </tr>
+                <tr>
+                    <td>Rank: </td>
+                    <td>{{ $user->rank }}</td>
+                </tr>
+                <tr>
+                    <td>Questions: </td>
+                    <td><a href="#">{{ $user->questions()->count() }}</a></td>
+                    {{-- TODO: Add route to user questions --}}
+                </tr>
+                <tr>
+                    <td>Answers: </td>
+                    <td><a href="#">{{ $user->answers()->count() }}</a></td>
+                    {{-- TODO: Add route to user answers --}}
+                </tr>
+            </tbody>
+        </table>
+        @if(Auth::user()->isAdmin || Auth::user()->id === $user->id)
+            <a href="{{ route('user.edit', [$user]) }}">Edit Profile</a>
+        @endif
+    </div>
 @endsection
